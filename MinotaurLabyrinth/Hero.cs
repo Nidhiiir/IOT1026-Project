a63c62b63d45ca07938f5215843228418f748933
Nidhiiir <131202460+Nidhiiir@users.noreply.github.com>
2023-06-25 20:11:31 -0400

    Update Hero.cs
    We have added health to the hero and added some function so that hero's health can be reduced

--- MinotaurLabyrinth/Hero.cs
+++ MinotaurLabyrinth/Hero.cs
@@ -1,34 +1,108 @@
-﻿namespace MinotaurLabyrinth
+namespace MinotaurLabyrinth
 {
-    // Represents the player in the game.
+    /// <summary>
+    /// Represents the player in the game.
+    /// </summary>
     public class Hero
     {
-        // Creates a new player that starts at the given location.
-        public Hero(Location start) => Location = start;
-        // Contains all the commands that a player can access.
+        private const int MaxHealth = 100;
+
+        /// <summary>
+        /// Creates a new player that starts at the given location.
+        /// </summary>
+        /// <param name="start">The starting location of the player.</param>
+        public Hero(Location start)
+        {
+            Location = start;
+            Health = MaxHealth;
+        }
+
+        /// <summary>
+        /// Contains all the commands that a player can access.
+        /// </summary>
         public CommandList CommandList { get; } = new CommandList();
-        // Represents the distance the player can sense danger.
-        // Diagonal adjacent squares have a range of 2 from the player.
+
+        /// <summary>
+        /// Represents the distance the player can sense danger.
+        /// Diagonally adjacent squares have a range of 2 from the player.
+        /// </summary>
         public int SenseRange { get; } = 1;
 
-        // The player's current location.
+        /// <summary>
+        /// The player's current location.
+        /// </summary>
         public Location Location { get; set; }
 
-        // Indicates whether the player is alive or not.
+        /// <summary>
+        /// Indicates whether the player is alive or not.
+        /// </summary>
         public bool IsAlive { get; private set; } = true;
 
-        // Indicated whether the player has won the game or not.
+        /// <summary>
+        /// Indicates whether the player has won the game or not.
+        /// </summary>
         public bool IsVictorious { get; set; }
 
-        // Indicates whether the player currently has the catacomb map.
+        /// <summary>
+        /// Indicates whether the player currently has the catacomb map.
+        /// </summary>
         public bool HasMap { get; set; } = true;
 
-        // Indicates whether the player currently has the sword.
+        /// <summary>
+        /// Indicates whether the player currently has the sword.
+        /// </summary>
         public bool HasSword { get; set; }
 
-        // Explains why a player died.
+        /// <summary>
+        /// Explains why a player died.
+        /// </summary>
         public string CauseOfDeath { get; private set; } = "";
 
+        /// <summary>
+        /// The current health of the player.
+        /// </summary>
+        public int Health { get; private set; }
+
+        /// <summary>
+        /// Adds the specified amount of health to the player.
+        /// </summary>
+        /// <param name="amount">The amount of health to add.</param>
+        public void AddHealth(int amount)
+        {
+            Health += amount;
+            if (Health > MaxHealth)
+            {
+                Health = MaxHealth;
+            }
+        }
+
+        /// <summary>
+        /// Removes the specified amount of health from the player.
+        /// </summary>
+        /// <param name="amount">The amount of health to remove.</param>
+        public void RemoveHealth(int amount)
+        {
+            Health -= amount;
+            if (Health <= 0)
+            {
+                Health = 0;
+                Kill("Health depleted");
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the current health of the player.
+        /// </summary>
+        /// <returns>The current health of the player.</returns>
+        public int GetHealth()
+        {
+            return Health;
+        }
+
+        /// <summary>
+        /// Kills the player with the specified cause of death.
+        /// </summary>
+        /// <param name="cause">The cause of death.</param>
         public void Kill(string cause)
         {
             IsAlive = false;
